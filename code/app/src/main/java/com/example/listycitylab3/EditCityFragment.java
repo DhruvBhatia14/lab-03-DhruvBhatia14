7e3575b95185db028592db4b2dcd2f10b16c31c8
Dhruv Bhatia <dhruvb1407@gmail.com>
2025-09-17 23:24:13 -0600

    Lab 3 working

---
+++ code/app/src/main/java/com/example/listycitylab3/EditCityFragment.java
@@ -0,0 +1,57 @@
+package com.example.listycitylab3;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class EditCityFragment extends DialogFragment {
+    interface EditCityDialogListener {
+        void EditCity(City city, String cityName, String province);
+    }
+
+    private EditCityFragment.EditCityDialogListener listener;
+    private final City editedCity;
+
+    public EditCityFragment(City editedCity) {
+        this.editedCity = editedCity;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof EditCityFragment.EditCityDialogListener) {
+            listener = (EditCityFragment.EditCityDialogListener) context;
+        } else {
+            throw new RuntimeException(context + " must implement EditCityDialogListener");
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.fragment_edit_city, null);
+        EditText editCityName = view.findViewById(R.id.edit_text_city_text);
+        editCityName.setText(this.editedCity.getName());
+        EditText editProvinceName = view.findViewById(R.id.edit_text_province_text);
+        editProvinceName.setText(this.editedCity.getProvince());
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        return builder
+                .setView(view)
+                .setTitle("Edit a city")
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton("Edit", (dialog, which) -> {
+                    String cityName = editCityName.getText().toString();
+                    String provinceName = editProvinceName.getText().toString();
+                    listener.EditCity(this.editedCity, cityName, provinceName);
+                })
+                .create();
+    }
+}
